cb289fec50f8019d09d6c79ee4bcf0ed051e72f2
Collins Ikpeyi <Ikpeyi89@gmail.com>
2022-09-11 20:02:59 -0500

    added the search functionality
    added the search functionality by fetch movie data from the api

--- src/App.js
+++ src/App.js
@@ -1,108 +1,36 @@
 import React, { useEffect } from "react";
-import { useState} from "react";
+import { useState } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 import MovieList from "./components/MovieList";
+import MovieListHeading from "./components/MovieListHeading";
+import SearchBox from "./components/SearchBox";
 
 function App() {
-  const [movies, setMovies] = useState([
-    {
-      Title: "Terminator 2: Judgment Day",
-      Year: "1991",
-      imdbID: "tt0103064",
-      Type: "movie",
-      Poster:
-        "https://m.media-amazon.com/images/M/MV5BMGU2NzRmZjUtOGUxYS00ZjdjLWEwZWItY2NlM2JhNjkxNTFmXkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_SX300.jpg",
-    },
-    {
-      Title: "The Terminator",
-      Year: "1984",
-      imdbID: "tt0088247",
-      Type: "movie",
-      Poster:
-        "https://m.media-amazon.com/images/M/MV5BYTViNzMxZjEtZGEwNy00MDNiLWIzNGQtZDY2MjQ1OWViZjFmXkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_SX300.jpg",
-    },
-    {
-      Title: "Terminator 3: Rise of the Machines",
-      Year: "2003",
-      imdbID: "tt0181852",
-      Type: "movie",
-      Poster:
-        "https://m.media-amazon.com/images/M/MV5BMTk5NzM1ODgyN15BMl5BanBnXkFtZTcwMzA5MjAzMw@@._V1_SX300.jpg",
-    },
-    {
-      Title: "Terminator Salvation",
-      Year: "2009",
-      imdbID: "tt0438488",
-      Type: "movie",
-      Poster:
-        "https://m.media-amazon.com/images/M/MV5BODBlOTJhZjItMGRmYS00YzM1LWFmZTktOTJmNDMyZTBjMjBkXkEyXkFqcGdeQXVyMjMwNDgzNjc@._V1_SX300.jpg",
-    },
-    {
-      Title: "Terminator Genisys",
-      Year: "2015",
-      imdbID: "tt1340138",
-      Type: "movie",
-      Poster:
-        "https://m.media-amazon.com/images/M/MV5BMjM1NTc0NzE4OF5BMl5BanBnXkFtZTgwNDkyNjQ1NTE@._V1_SX300.jpg",
-    },
-    {
-      Title: "Terminator: Dark Fate",
-      Year: "2019",
-      imdbID: "tt6450804",
-      Type: "movie",
-      Poster:
-        "https://m.media-amazon.com/images/M/MV5BNzhlYjE5MjMtZDJmYy00MGZmLTgwN2MtZGM0NTk2ZTczNmU5XkEyXkFqcGdeQXVyMTkxNjUyNQ@@._V1_SX300.jpg",
-    },
-    {
-      Title: "Terminator: The Sarah Connor Chronicles",
-      Year: "2008–2009",
-      imdbID: "tt0851851",
-      Type: "series",
-      Poster:
-        "https://m.media-amazon.com/images/M/MV5BZGE2ZDgyOWUtNzdiNS00OTI3LTkwZGQtMTMwNzM4YWUxNGNhXkEyXkFqcGdeQXVyNjU2NjA5NjM@._V1_SX300.jpg",
-    },
-    {
-      Title: "Terminator 3: Rise of the Machines",
-      Year: "2003",
-      imdbID: "tt0364056",
-      Type: "game",
-      Poster:
-        "https://m.media-amazon.com/images/M/MV5BMjA5OTk4MTQwNV5BMl5BanBnXkFtZTgwMzkxNTEwMTE@._V1_SX300.jpg",
-    },
-    {
-      Title: "Lady Terminator",
-      Year: "1989",
-      imdbID: "tt0095483",
-      Type: "movie",
-      Poster:
-        "https://m.media-amazon.com/images/M/MV5BMTg5NTA1NzEtNWNiNy00ZTc4LWJhZTgtYmJkODZhYWI3NmQ4XkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_SX300.jpg",
-    },
-    {
-      Title: "Terminator II",
-      Year: "1989",
-      imdbID: "tt0098321",
-      Type: "movie",
-      Poster:
-        "https://m.media-amazon.com/images/M/MV5BNDNlMWExNTMtMmJkYi00OTg2LTlmY2EtZThjYzA1ZWI2NzFkXkEyXkFqcGdeQXVyMzM4MjM0Nzg@._V1_SX300.jpg",
-    },
-  ]);
+  const [movies, setMovies] = useState([]);
 
-  const getMovieRequest = async () => {
-    const url = "http://www.omdbapi.com/?s=terminator&apikey=d887ba1a";
+  const [searchValue, setSearchValue] = useState("");
+
+  const getMovieRequest = async (searchValue) => {
+    const url = `http://www.omdbapi.com/?s=${searchValue}&apikey=d887ba1a`;
 
     const response = await fetch(url);
     const responseJson = await response.json();
-
-    console.log(responseJson);
+    if (responseJson.Search) {
+      setMovies(responseJson.Search);
+    }
   };
 
   useEffect(() => {
-    getMovieRequest();
-  }, []);
+    getMovieRequest(searchValue);
+  }, [searchValue]);
 
   return (
     <div className="container-fluid movie-app">
+      <div className="row d-flex align-items-center m-4">
+        <MovieListHeading heading="Movies" />
+        <SearchBox searchValue={searchValue} setSearchValue={setSearchValue} />
+      </div>
       <div className="row">
         <MovieList movies={movies} />
       </div>
